0f6f9d177100bce56ab344bea26b31bec7c4cd59
piero de salvia <pierods@users.noreply.github.com>
2019-04-09 22:42:45 +0200

    First draft - fetch an URL

--- app/src/main/java/com/github/pierods/aeslens/MainActivity.java
+++ app/src/main/java/com/github/pierods/aeslens/MainActivity.java
@@ -4,41 +4,56 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final TextView decodedText = (TextView) findViewById(R.id.decodedText);
+        final EditText urlText = (EditText) findViewById(R.id.urlEditText);
+
+        urlText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    decodedText.setText(retrieveURL(urlText.getText().toString()));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
+    private String retrieveURL(String urlText) {
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            URL url = new URL(urlText);
+
+            BufferedReader in;
+            in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                sb.append(inputLine);
+
+            in.close();
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            return e.getLocalizedMessage();
+        }
+    }
 }
